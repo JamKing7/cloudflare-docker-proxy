877d52ed92771a079f19612d71834ede9f67874c
JamKing7 <84217868+JamKing7@users.noreply.github.com>
2024-06-09 16:13:23 +0800

    Update index.js
    change domain

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.jamking.top": "https://registry-1.docker.io",
+  "quay.jamking.top": "https://quay.io",
+  "gcr.jamking.top": "https://gcr.io",
+  "k8s-gcr.jamking.top": "https://k8s.gcr.io",
+  "k8s.jamking.top": "https://registry.k8s.io",
+  "ghcr.jamking.top": "https://ghcr.io",
+  "cloudsmith.jamking.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
